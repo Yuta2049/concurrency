5da87b3f5a4b9e05288d78ad5e4f5138c73afa42
Тараруева Юлия Николаевна <yuliya.tararueva@rtlabs.ru>
2023-11-18 23:22:32 +0800

    m3 restaurant

---
+++ src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
@@ -0,0 +1,32 @@
+package course.concurrency.m3_shared.collections;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+public class RestaurantService {
+
+    private Map<String, Restaurant> restaurantMap = new ConcurrentHashMap<>() {{
+        put("A", new Restaurant("A"));
+        put("B", new Restaurant("B"));
+        put("C", new Restaurant("C"));
+    }};
+
+    private Map<String, Integer> stat = new ConcurrentHashMap<>();
+
+    public Restaurant getByName(String restaurantName) {
+        addToStat(restaurantName);
+        return restaurantMap.get(restaurantName);
+    }
+
+    public void addToStat(String restaurantName) {
+        stat.merge(restaurantName, 1, Integer::sum);
+    }
+
+    public Set<String> printStat() {
+        return stat.entrySet().stream()
+                .map(entry -> entry.getKey()  + " - " + entry.getValue())
+                .collect(Collectors.toSet());
+    }
+}
